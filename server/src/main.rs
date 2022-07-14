d9d42a87e74821ab3b3326cac1fea9fa9f8a5c4a
Emils <emils@mullvad.net>
2022-07-14 17:41:32 +0100

    Make websocket stub

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,25 @@
 use bytes::Buf;
-use tokio::fs::File;
-use tokio::io::AsyncWriteExt;
-use tokio_stream::StreamExt;
+use futures::{SinkExt, StreamExt};
+use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
+use tokio::{fs::File, io::BufReader};
+// use tokio_stream::StreamExt;
 use warp::{self, Filter, Rejection, Reply, Stream};
 
 #[tokio::main]
 async fn main() {
+    let (button_tx, _button_rx) = tokio::sync::broadcast::channel(1);
+    let event_tx = button_tx.clone();
+    tokio::spawn(async move {
+        // Get tokio's version of stdin, which implements AsyncRead
+        let stdin = tokio::io::stdin();
+        // Create a buffered wrapper, which implements BufRead
+        let reader = BufReader::new(stdin);
+        // Take a stream of lines from this
+        let mut lines = reader.lines();
+        while let Ok(Some(_next_line)) = lines.next_line().await {
+            let _ = button_tx.send(true);
+        }
+    });
 
     let receiver = warp::path!("recording")
         .and(warp::filters::path::end())
@@ -17,9 +31,39 @@ async fn main() {
         .and(warp::filters::method::get())
         .and_then(index);
 
+    let events = warp::path!("events")
+        .and(warp::filters::ws::ws())
+        .and(warp::any().map(move || event_tx.clone()))
+        .and_then(
+            |ws: warp::filters::ws::Ws, event_tx: tokio::sync::broadcast::Sender<bool>| async move {
+                let event_tx = event_tx.clone();
+                let mut socket_rx =
+                    tokio_stream::wrappers::BroadcastStream::new(event_tx.subscribe()).fuse();
+                Result::<_, Rejection>::Ok(ws.on_upgrade(move |ws| async move {
+                    let mut ws = ws.fuse();
+                    futures::select! {
+                        btn = socket_rx.select_next_some() => {
+                            if btn == Ok(true) {
+                                if let Err(err) = ws.send(warp::ws::Message::text("p")).await {
+                                    println!("websocket failed: {}", err);
+                                    return;
+                                }
+                            }
+
+                        },
+                        rx_msg = ws.next() => {
+                            if rx_msg.is_none() {
+                                return;
+                            }
+                        },
+                    };
+                }))
+            },
+        );
+
     println!("Serving the video guest book booth");
 
-    warp::serve(receiver.or(index))
+    warp::serve(receiver.or(index).or(events))
         .run("127.0.0.1:8080".parse::<std::net::SocketAddr>().unwrap())
         .await;
 }
